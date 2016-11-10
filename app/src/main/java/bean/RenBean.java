8138c63d7fd94f049700d38d8bbc8d9e74ec8ba6
chengkuo <597863395@qq.com>
2016-11-10 22:18:48 +0800

    我就来试试可以上传吗

---
+++ app/src/main/java/bean/RenBean.java
@@ -0,0 +1,45 @@
+package bean;
+
+/**
+ * Created by a452542253 on 2016/11/10.
+ */
+
+public class RenBean {
+    private String url_touxiang;
+    private String name;
+    private String qianming;
+    private String shijian_didian;
+
+    public String getUrl_touxiang() {
+        return url_touxiang;
+    }
+
+    public void setUrl_touxiang(String url_touxiang) {
+        this.url_touxiang = url_touxiang;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQianming() {
+        return qianming;
+    }
+
+    public void setQianming(String qianming) {
+        this.qianming = qianming;
+    }
+
+    public String getShijian_didian() {
+        return shijian_didian;
+    }
+
+    public void setShijian_didian(String shijian_didian) {
+        this.shijian_didian = shijian_didian;
+    }
+
+}
